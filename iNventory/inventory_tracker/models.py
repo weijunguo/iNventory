021721a9c30a2a6d9fe311dc5f6d7b2b3846b2ef
Xiaoou Wang <xiaooududu@gmail.com>
2014-02-17 19:27:13 -0500

    view update

--- iNventory/inventory_tracker/models.py
+++ iNventory/inventory_tracker/models.py
@@ -1,6 +1,43 @@
 from django.db import models
+import ast, re
 
-    
+def list_to_str(l):
+
+    r = ''
+    for i in l:
+        r = r + i + ','
+
+    return r[:-1]
+
+class ListField(models.TextField):
+    __metaclass__ = models.SubfieldBase
+    description = "Stores a python list"
+
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        if value.find('[') == -1:
+            return re.split('\s*,\s*', value)
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+
+
 class SalesPerson(models.Model):
     name = models.CharField(max_length=200)
 
@@ -31,13 +68,15 @@ class Vehicle(models.Model):
                               choices=STATUS,
                               default='RT',
                               db_index=True)
-    inventory_type = models.CharField(max_length=64,
+    inventory_type = models.CharField('Inventory Type',
+                                      max_length=64,
                                       choices=TYPE,
                                       default='NEW',
                                       db_index=True)
     year = models.IntegerField(db_index=True)
+    brand = models.CharField(max_length=64, default="BMW")
     model = models.CharField(max_length=64, db_index=True)
-    vin = models.CharField(max_length=64, db_index=True)
+    vin = models.CharField('VIN', max_length=64, db_index=True, unique=True)
     serial = models.CharField(max_length=64, db_index=True)
     stock = models.CharField(max_length=64, db_index=True)
     exterior = models.CharField(max_length=64, db_index=True)
@@ -46,8 +85,8 @@ class Vehicle(models.Model):
                                     choices=TRANSMISSION,
                                     default='A',
                                     db_index=True)
-    options = models.CharField(max_length=10000)
-    miles = models.IntegerField(db_index=True)
+    options = ListField(max_length=10000)
+    miles = models.IntegerField('Milage', db_index=True)
     retail = models.DecimalField(max_digits=10, 
                                  decimal_places=2,db_index=True)
     invoice = models.DecimalField(max_digits=10, 
@@ -59,6 +98,13 @@ class Vehicle(models.Model):
     detail = models.CharField(max_length=1000000, 
                               blank=True,
                               null=True)
+    
 
+    def __unicode__(self):
+        return self.description()
 
-# Create your models here.
+    def description(self):
+        return str(self.year) + " " + self.brand + " " + self.model
+
+    def configuration(self):    
+        return self.exterior + '/' + self.interior + '/' + list_to_str(self.options)
