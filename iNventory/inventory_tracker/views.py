eded51eba48690b9ed410ce0948cb5aea2e7ee2e
Xiaoou Wang <xiaooududu@gmail.com>
2014-02-17 20:34:31 -0500

    my work

--- iNventory/inventory_tracker/views.py
+++ iNventory/inventory_tracker/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.template import loader, RequestContext
-from models import SalesPerson, Vehicle
+from models import Vehicle
 
 def index(request):
-    all_objects = Vehicle.objects.all()
+    all_vehicles_list = Vehicle.objects.order_by('-year')[:5]
     template = loader.get_template('inventory_tracker/index.html')
     context = RequestContext(request, {
-            'all_objects': all_objects,
+            'all_vehicles_list': all_vehicles_list,
             })
     return HttpResponse(template.render(context))
